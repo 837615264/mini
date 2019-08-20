30cd5bf159391ab888ddd9afbe49fcb2fbb91819
837615264 <837615264@qq.com>
2019-08-20 15:45:05 +0800

    test

--- config/ind_db.php
+++ config/ind_db.php
@@ -2,7 +2,7 @@
 defined('IN_IA') or define('IN_IA', true);
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;port=3306;dbname=mini_cnlnx_com',
+    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=mini_cnlnx_com',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
